f02b87f3f1769a7ee2bedb4f3a11cd2cc61fbc4d
Pedro Costa <pedro.costa@pauatech.com>
2024-03-11 14:45:16 +0000

    Exercise 5.1 - Dismiss all toast messages when clicking the "Escape" key

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,6 +5,17 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    function handleDismissToast(event) {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    }
+    window.addEventListener('keydown', handleDismissToast);
+
+    return () => window.removeEventListener('keydown', handleDismissToast);
+  }, []);
+
   function createToast({ message, variant }) {
     const nextToasts = [
       ...toasts,
